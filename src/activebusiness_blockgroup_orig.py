0d903440152be83a5e438471328a433ac8d3c203
tiffanychu90 <tiffany.chu@lacity.org>
2021-07-29 15:24:46 -0700

    Cleanup and remove testing notebooks

--- src/activebusiness_blockgroup_orig.py
+++ src/activebusiness_blockgroup_orig.py
@@ -1,200 +1,275 @@
 # Source: https://github.com/prestinomills/aqueduct/blob/Know_Your_Community_Pipelines/civis/geohub/ActiveBusinessBlockgroupAggregation/Active_Business_Finalized_Script.py
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 # %load active_business_script.py
 """
 Created on Wed May  1 08:51:03 2019
 @author: myrfid041
 """
-
-import os
-pwd=os.getcwd()
-print(pwd)
-import sys
-#!{sys.executable} -m pip install sodapy
-from sodapy import Socrata
-import pandas as pd
-import numpy as np
-#!{sys.executable} -m pip install geopandas
 import geopandas as gpd
-#import  install altair.vegalite.v2 as alt
-import folium
-import xlsxwriter
-from shapely.geometry import Point
-#!{sys.executable} -m pip install arcgis
+import os
+import pandas as pd
+
+import utils
+from civis_aqueduct_utils.github import upload_file_to_github
+
+from sodapy import Socrata
 from arcgis.gis import GIS
 from arcgis.features.summarize_data import join_features
-import json
-import credentials
-from IPython.display import display
-from arcgis.features import FeatureLayer
-from arcgis.features import FeatureLayerCollection
-import json
+from arcgis.features import FeatureLayer, FeatureLayerCollection
 from copy import deepcopy
-import intake_civis
+from IPython.display import display
 
 lahub_user = os.environ["LAHUB_ACC_USERNAME"]
 lahub_pass = os.environ["LAHUB_ACC_PASSWORD"]
 
-
 #---Setting the Outputs
-ABOutput=pwd+'/Listing_of_Active_Businesses.csv'
+OUTPUT_FILE = "./Listing_of_Active_Businesses.csv"
+output_layer_name = "a7236cc62ded454c94a64e9d80d6304a"
 
-
-
-#---Pulling Active Business Data
+# Grab the Socrata dataset for active businesses
 client = Socrata("data.lacity.org", None)
-abiz = pd.DataFrame(client.get('ngkp-kqkn', limit=10000000))
+abiz = pd.DataFrame(client.get('ngkp-kqkn', limit=10_000_000))
+
+# Constants for loading the file to GitHub
+TOKEN = os.environ["GITHUB_TOKEN_PASSWORD"]
+REPO = "CityOfLosAngeles/data-team-wiki"
+BRANCH = "master"
+
+DEFAULT_COMMITTER = {
+    "name": "Los Angeles ITA data team",
+    "email": "ITAData@lacity.org",
+}
 
 
+def dataprep(df, branch="master"):
 
-#---Pull NAIC Industry Table
-n_table='https://raw.githubusercontent.com/prestinomills/aqueduct/Know_Your_Community_Pipelines/civis/geohub/ActiveBusinessBlockgroupAggregation/naics_industry_table.csv'
-naics_table=pd.read_csv(n_table)
+    # Pull NAICS Industry Table
+    n_table=(
+        'https://raw.githubusercontent.com/CityofLosAngeles/civis-gcp-transition/{}/'
+        'data/naics_industry_table.csv'
+    )
+    naics_table=pd.read_csv(n_table.format(branch))
 
+    # Grab location info
+    df = (df.dropna(subset=['location_1', 'naics'])
+        .assign(
+            location_2 = df.location_1.astype(str).str[34:-2]
+        )
+    )
 
-def chunks(l, n, z):
-    """Yield successive n-sized chunks from l."""
-    for i in range(0, len(l), n):
-        chunk_list=l[i:i + n]
-        z.edit_features(updates= chunk_list)
-        print("update successful")
+    df = df.assign(
+        longitude = df.location_2.str.split(",", expand=True)[0].astype(float),
+        latitude = df.location_2.str.split(",", expand=True)[1].astype(float),
+        naics_sector = df.naics.str[:2].astype(str),
+    ).dropna(subset=["longitude", "latitude"])
 
+    # Merge in NAICS sector
+    df2 = pd.merge(df, 
+                   naics_table.assign(
+                       naics_sector = naics_table.naics_sector.astype(str)
+                   ), 
+            how = 'inner', on = 'naics_sector', validate = 'm:1'
+            )
 
-def abOutput(x):
-    x.to_csv(ABOutput)
-
-
-def top10(x):
-    x.pred_ind=x.idxmax(axis=1)
-    predom_industries=pd.DataFrame(x.pred_ind.value_counts())[0:12]
-    predom_industries=predom_industries.index.tolist()
-    predom_industries.remove('Professional, Scientific, and Technical Services');
-    predom_industries.remove('Other Services (except Public Administration)');
-    return predom_industries
-
-def dataprep(x,y):
-    df=x
-    df=df.dropna(subset=['location_1','naics'])
-    df['location_2']=df['location_1'].astype('str') #Prepping location data to parse (x,y) values
-    df['location_2']=df['location_2'].str[34:-2]
-    locations = df["location_2"].str.split(",", n = 1, expand = True) #Creating a dataframe with x,y coordinates
-    locations[1] =locations[1].str[1:]
-    df['lon']=locations[0]
-    df['lat']=locations[1]
-    df=df.dropna(subset=['lat','lon'])
-    df['naics_sector'] = df['naics'].str[:2].astype('str')
-    dfn=y
-    dfn['naics_sector']=dfn['naics_sector'].astype('str')
-    df2=pd.merge(df,dfn,how='inner',on='naics_sector',validate='m:1')
-    df2.lon=df.lon.astype(float)
-    df2.lat=df.lat.astype(float)
     # Create geometry column
-    df2['geometry'] = df2.apply(
-        lambda row: Point(row.lon, row.lat), axis=1)
-    # Rename columns
-    df2.rename(columns = {'lon': 'longitude', 'lat':'latitude'}, inplace=True)
-    df2=df2.dropna(subset=['longitude','latitude'])
-    gdf = gpd.GeoDataFrame(df2, geometry = 'geometry')
-    # Set CRS
-    gdf.crs = {'init':'epsg:4326'}
-    # Drop NAs, then project to CA State Plane
-    gdf = gdf[gdf.geometry.notna()]
-    gdf = gdf.to_crs({'init':'epsg:2229'})
-    block = gpd.read_file('./LACounty_Blockgroup/')
-    block.crs={'init':'epsg:2229'}
-    sjoin=gpd.sjoin(gdf,block,how='inner',op='intersects')
-    sjoin2=sjoin.pivot_table(index='GEOID10',values='business_name',columns=['naics_industry'],aggfunc=len)
-    sjoin2=sjoin2.fillna(0)
-    abOutput(sjoin2)
-    predom_ind=top10(sjoin2)
-    return predom_ind
+    gdf = gpd.GeoDataFrame(df2.dropna(subset=['longitude', 'latitude']), 
+        geometry = gpd.points_from_xy(df2.longitude, df2.latitude),
+                                      crs = "EPSG:4326"
+    ).to_crs("EPSG:2229") # Change to CA State Plane
+
+    # Import block groups
+    block_group_file=(
+        'https://raw.githubusercontent.com/CityofLosAngeles/civis-gcp-transition/{}/'
+        'data/LACounty_Blockgroup.geojson'
+    )
+    block = gpd.read_file(block_group_file.format(branch))
+
+    # Aggregate
+    sjoin=gpd.sjoin(gdf, block, how='inner', op='intersects')
+    
+    sjoin = sjoin.assign(
+        GEOID10 = sjoin.GEOID10.astype(str).apply(lambda x: '{0:0>12}'.format(x))
+    )
+
+    sjoin2=(sjoin.pivot_table(index='GEOID10', 
+                    values='business_name',
+                    columns=['naics_industry'], 
+                    aggfunc=len)
+        .reset_index()
+        .fillna(0)
+        .rename_axis(None, axis="columns")
+    )
+    
+    return sjoin2
 
 
-def go():
-    predom=dataprep(abiz,naics_table)
-    updated_csv_df = pd.read_csv(ABOutput)
-    updated_csv_df['GEOID10']=updated_csv_df['GEOID10'].astype(str)
-    updated_csv_df.dtypes
-    updated_csv_df['GEOID10'] = updated_csv_df['GEOID10'].apply(lambda x: '{0:0>12}'.format(x))
-    geohub_updates(updated_csv_df,credentials.lahub_user,credentials.lahub_pass,predom)
+def top10(df):
+    '''
+    Find the top 10 predominant industries in entire county
+    Exclude 2 categories
+    Return a list (used to update feature layer item property)
+    df: pandas.DataFrame. Use the output returned from `dataprep()`.
+    '''
+    
+    county_aggregate = (
+        pd.DataFrame(df.set_index("GEOID10")
+                     .idxmax(axis=1))
+        .reset_index()
+        .rename(columns = {0: "predominant_industry"})
+    )
+    
+    # Get a list, descending order
+    predominant_industries = (county_aggregate.predominant_industry.value_counts()
+                              .index
+                              .to_list()
+                             )
+    
+    # Exclude these categories, then grab top 10
+    exclude_me = ['Professional, Scientific, and Technical Services', 
+              'Other Services (except Public Administration)']
+    for i in exclude_me:
+        predominant_industries.remove(i)
+    
+    top10_industries = predominant_industries[0:10]
+    
+    return top10_industries
 
 
-def update_desc(x,y,z):
-    active_biz=z.content.get(y)    
+    '''
+ESRI stores the column names slightly differently (subject to 10 char limits)
+Use dict to map and rename (key-value pair)
+Key: dataframe's existing column name
+Value: ESRI column name
+'''
+LAYER_RENAME_COLUMNS_DICT = {
+    'Accommodation and Food Services': 'Accommodation_and_Food_Services',
+    'Administrative and Support and Waste Management and Remediation Services': 'Administrative_and_Support_and_',
+    'Agriculture, Forestry, Fishing and Hunting': 'Agriculture__Forestry__Fishing_',
+    'Arts, Entertainment, and Recreation': 'Arts__Entertainment__and_Recrea',
+    'Construction': 'Construction',
+    'Educational Services': 'Educational_Services',
+    'Finance and Insurance': 'Finance_and_Insurance',
+    'Health Care and Social Assistance': 'Health_Care_and_Social_Assistan',
+    'Information': 'Information',
+    'Manufacturing': 'Manufacturing',
+    'Medical Marijuana Collective': 'Medical_Marijuana_Collective',
+    'Mining': 'Mining',
+    'Not Classified': 'Not_Classified',
+    'Other Services (except Public Administration)': 'Other_Services__except_Public_A',
+    'Professional, Scientific, and Technical Services': 'Professional__Scientific__and_T',
+    'Real Estate Rental and Leasing': 'Real_Estate_Rental_and_Leasing',
+    'Retail Trade': 'Retail_Trade',
+    'Transportation and Warehousing': 'Transportation_and_Warehousing',
+    'Utilities': 'Utilities',
+    'Wholesale Trade': 'Wholesale_Trade'                                               
+}
+
+
+def geohub_updates(df,user,pas, feature_layer_id, 
+        top10_industries, column_renaming_dict, OUTPUT_FILE):
+    """
+    df: pandas.DataFrame. Input the df that is returned from `dataprep()`
+    user: str. AGOL username
+    pas: str. AGOL password
+    feature_layer_id: str. The feature layer ID of the AGOL layer.
+    top10_industries: list.
+    column_renaming_dict: dict. The dictionary to map our df column names to how it's stored in AGOL.
+    OUTPUT_FILE: str. The path to where the local CSV is stored, will be checked into GitHub.
+    """
+    gis = GIS('https://lahub.maps.arcgis.com', username=user, password=pas)
+    flayer = gis.content.get(feature_layer_id)
+    ActiveBusinesses_flayer = flayer.layers[0]
+    ActiveBusinesses_fset = ActiveBusinesses_flayer.query() #querying without any conditions returns all the features    
+    
+    # Grab the spatial dataframe in the layer
+    # Drop the GEOID10s that don't have a match in the existing sdf
+    # Only keep the columns to update (various industries), and leave geometry columns intact
+    existing_table = ActiveBusinesses_fset.sdf
+    
+    industry_cols = list(column_renaming_dict.values())
+    
+    new_updated_table = (df[df.GEOID10.isin(existing_table.GEOID10)]
+                         .rename(columns = column_renaming_dict)
+                     [["GEOID10"] + industry_cols]
+                    )
+    new_updated_table[industry_cols] = new_updated_table[industry_cols].fillna(0).astype(int)
+    
+    updated_values_dict = new_updated_table.set_index("GEOID10").to_dict(orient="index")
+    
+    # Grab the features, save as a list. It's actually a list, but each element in the list is a dictionary.
+    feature_list = ActiveBusinesses_fset.features
+   
+    # Loop through each GEOID, then loop through the columns, and update the values
+    # Use try/except because there are some GEOIDs in the original sdf that aren't in our df (about 10ish)
+    # Leave these with original values, since we don't have updated values
+    updated_features = []
+    for i in range(0, len(feature_list)):
+        original_feature = [f for f in feature_list][i]
+        feature_to_be_updated = deepcopy(original_feature)
+        attributes_to_update = feature_to_be_updated.attributes
+        geoid = attributes_to_update["GEOID10"]
+        try:
+            for col in list(column_renaming_dict.values()):
+                '''
+                This doesn't work: 
+                feature_to_be_updated[col] = updated_values_dict[geoid][col]
+
+                feature_to_be_updated[col] = features object...not a dict
+                print(type(feature_list[0]))
+                print(type(feature_list[0].attributes))
+
+                Somehow, it won't let us key into the dict and change values
+                But, since we earlier got into the attributes, we can update the values for each attribute
+                https://gis.stackexchange.com/questions/394259/esri-notebook-select-and-append-records-to-table
+                '''
+                attributes_to_update[col] = int(updated_values_dict[geoid][col])
+            updated_features.append(feature_to_be_updated)
+
+        except:
+            pass
+
+    # Update the feature layer in chunks of 1000 rows
+    utils.chunks(updated_features, 1000, ActiveBusinesses_flayer)
+
+    # Update the AGOL item properties
     text = """
-    This layer is aggregating <a href="https://data.lacity.org/A-Prosperous-City/Listing-of-Active-Businesses/6rrh-rzua">Listing of Active Businesses Data</a> that have geospatial information associated. The top 10 most frequent industries in block groups are:
+    This layer is aggregating 
+    <a href="https://data.lacity.org/A-Prosperous-City/Listing-of-Active-Businesses/6rrh-rzua">
+    Listing of Active Businesses Data</a> 
+    that have geospatial information associated. 
+    The top 10 most frequent industries in block groups are:
     {}
     """
-    
+    # In list of frequent industries, let's display as a string, not a list
+    x = ', '.join([str(elem) for elem in top10_industries]) 
     item_props = {'title' : 'Active Businesses Data by Block Group', 'description':text.format(x)}
-    active_biz.update(item_properties=item_props)
-    print("updates made!")
+    flayer.update(item_properties=item_props)
+    print("item properties updated!")
+    
+    # Stage the file to check into GitHub
+    new_updated_table.to_csv(OUTPUT_FILE, index=False)
 
 
-def geohub_updates(x,user,pas,topz):
-    gis = GIS('https://lahub.maps.arcgis.com',  username=user, password=pas)
-    output_layer_name = '067a9242fbef4afeb1ca0744952e5724'
-    actbus=gis.content.search(output_layer_name)
-    ActiveBusinesses_item = actbus[0]
-    ActiveBusinesses_flayer = ActiveBusinesses_item.layers[0]
-    ActiveBusinesses_flayer
-    ActiveBusinesses_fset = ActiveBusinesses_flayer.query() #querying without any conditions returns all the features
-    ActiveBusinesses_fset.sdf.head()
-    ActiveBusinesses_fset.sdf.shape
-    ActiveBusinesses_fset.sdf.dtypes
-    overlap_rows = pd.merge(left = ActiveBusinesses_fset.sdf, 
-                        right = x, 
-                        how='inner',
-                        on = 'GEOID10')
-    overlap_rows.head(5)
-
-    # overlap_rows.to_csv("C:\\Users\\mad10412\\Desktop\\Merged.csv")
-    overlap_rows.shape
-
-    #Perform updates
-
-    features_for_update = [] #list containing corrected features
-    all_features = ActiveBusinesses_fset.features
-
-    for GEOID10 in overlap_rows['GEOID10']:
-        # get the feature to be updated
-        original_feature = [f for f in all_features if f.attributes['GEOID10'] == GEOID10][0]
-        feature_to_be_updated = deepcopy(original_feature)
-
-        # get the matching row from csv
-        matching_row = x.where(x.GEOID10 == GEOID10).dropna()
-
-        # assign the updated values
-        feature_to_be_updated.attributes['Accommodation_and_Food_Services'] = matching_row['Accommodation and Food Services'].values[0]
-        feature_to_be_updated.attributes['Administrative_and_Support_and_'] = matching_row['Administrative and Support and Waste Management and Remediation Services'].values[0]
-        feature_to_be_updated.attributes['Agriculture__Forestry__Fishing_'] = matching_row['Agriculture, Forestry, Fishing and Hunting'].values[0]
-        feature_to_be_updated.attributes['Arts__Entertainment__and_Recrea'] = matching_row['Arts, Entertainment, and Recreation'].values[0]
-        feature_to_be_updated.attributes['Construction'] = matching_row['Construction'].values[0]
-        feature_to_be_updated.attributes['Educational_Services'] = matching_row['Educational Services'].values[0]
-        feature_to_be_updated.attributes['Finance_and_Insurance'] = matching_row['Finance and Insurance'].values[0]
-        feature_to_be_updated.attributes['Health_Care_and_Social_Assistan'] = matching_row['Health Care and Social Assistance'].values[0]
-        feature_to_be_updated.attributes['Information'] = matching_row['Information'].values[0]
-        feature_to_be_updated.attributes['Manufacturing'] = matching_row['Manufacturing'].values[0]
-        feature_to_be_updated.attributes['Medical_Marijuana_Collective'] = matching_row['Medical Marijuana Collective'].values[0]
-        feature_to_be_updated.attributes['Mining'] = matching_row['Mining'].values[0]
-        feature_to_be_updated.attributes['Not_Classified'] = matching_row['Not Classified'].values[0]
-        feature_to_be_updated.attributes['Other_Services__except_Public_A'] = matching_row['Other Services (except Public Administration)'].values[0]
-        feature_to_be_updated.attributes['Professional__Scientific__and_T'] = matching_row['Professional, Scientific, and Technical Services'].values[0]
-        feature_to_be_updated.attributes['Real_Estate_Rental_and_Leasing'] = matching_row['Real Estate Rental and Leasing'].values[0]
-        feature_to_be_updated.attributes['Retail_Trade'] = matching_row['Retail Trade'].values[0]
-        feature_to_be_updated.attributes['Transportation_and_Warehousing'] = matching_row['Transportation and Warehousing'].values[0]
-        feature_to_be_updated.attributes['Utilities'] = matching_row['Utilities'].values[0]
-        feature_to_be_updated.attributes['Wholesale_Trade'] = matching_row['Wholesale Trade'].values[0]
-
-        #add this to the list of features to be updated
-        features_for_update.append(feature_to_be_updated)
-    chunks(features_for_update, 1000,ActiveBusinesses_flayer)
-    update_desc(topz,output_layer_name,gis)
-
-
-get_ipython().run_cell_magic('time', '', 'if __name__ == "__main__":\n\tgo()')
+# Run it all
+if __name__ == "__main__":
+    # Import data and clean
+    df = dataprep(abiz, branch="active-business")
+    # Grab top 10 industries as a list
+    top10_industries = top10(df)
+    # Update AGOL, export local CSV
+    geohub_updates(df,lahub_user,lahub_pass, output_layer_name, 
+        top10_industries, LAYER_RENAME_COLUMNS_DICT, OUTPUT_FILE)
+    
+    '''
+    # Upload to GitHub
+    upload_file_to_github(
+        TOKEN,
+        REPO,
+        BRANCH,
+        f"ActiveBusinessBlockgroupAggregation/{OUTPUT_FILE.replace('./', '')}",
+        f"{OUTPUT_FILE}",
+        f"Update Active Businesses",
+        DEFAULT_COMMITTER,
+    )
+    
+    os.remove(OUTPUT_FILE)
+    '''
